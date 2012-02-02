2d4ebdc3f498a81343a9270b44cc4d04d3c13ebc
John Li <ivan.lishchinovskiy@gmail.com>
2012-02-02 23:20:22 +0200

    added some style

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,12 +36,27 @@ class PostsController < ApplicationController
 	end
 
 	def update
+		@post = Post.find(params[:id])
 
+		respond_to do |format|
+      if @post.update_attributes(params[:post])
+        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
 	end
 
 	def destroy
 		@post = Post.find(params[:id])
 		@post.destroy
+
+		respond_to do |format|
+      format.html { redirect_to posts_url }
+      format.json { head :no_content }
+    end
 	end	
 
 end
