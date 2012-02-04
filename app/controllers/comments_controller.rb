69602f911fb9726e31fed04e133d52e1f681b007
John Li <ivan.lishchinovskiy@gmail.com>
2012-02-04 22:36:04 +0200

    comments error messages form fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,12 @@ class CommentsController < ApplicationController
 	def create
 		@post = Post.find(params[:post_id])
 		@comment = @post.comments.create(params[:comment])
-		redirect_to post_path(@post)
+
+		if @comment.valid?
+		 	redirect_to post_path(@post)
+		else
+			render "posts/show"
+		end
 	end
 
 	def destroy
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,11 +6,9 @@ class PostsController < ApplicationController
 
 	def show
 		@post = Post.find(params[:id])
+		@comments = @post.comments
+		@comment = @post.comments.build
 
-		respond_to do |format|
-	      format.html # show.html.erb
-	      format.json { render json: @post }
-	    end
 	end
 
 	def new
@@ -24,14 +22,10 @@ class PostsController < ApplicationController
 	def create
 		@post = Post.new(params[:post])
 	
-		respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render json: @post, status: :created, location: @post }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.' 
+    else
+      render  :new 
     end
 	end
 
