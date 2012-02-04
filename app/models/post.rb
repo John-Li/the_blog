6846db97cd53de819d872dbbdc8e38aa68f23817
John Li <ivan.lishchinovskiy@gmail.com>
2012-02-04 11:27:15 +0200

    added post validations & error messages

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,9 @@
 class Post < ActiveRecord::Base
+
+	validates :name, presence: true
+	validates :title, presence: true, length: { minimum: 5 }
+	validates :content, presence: true
+
+	has_many :comments, dependent: :destroy
+
 end
