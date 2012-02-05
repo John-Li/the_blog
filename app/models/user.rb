b985bbed446a9bb73a9a84136ecdacd302e3e85a
John Li <ivan.lishchinovskiy@gmail.com>
2012-02-05 15:52:50 +0200

    added some security to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ActiveRecord::Base
-	attr_accessible :name, :email
+	attr_accessible :name, :email, :password, :password_confirmation
+	has_secure_password
 
 	validates :name,  presence: true, length: { maximum: 40 }
+
 	valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: valid_email_regex },
 										uniqueness: { case_sensetive: false }
+
+	validates :password, length: { minimum: 6 }
+	validates :password_digest, presence: { message: "Password can't be blank" }
 end
